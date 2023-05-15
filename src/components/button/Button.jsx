073950677f9af78e1c9ca974140241f2fb029294
nazarena <epet20nazarenafalleti@gmail.com>
2023-05-15 16:08:09 -0300

    prueba de boton

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,1 +1,17 @@
+import React, { Component } from 'react';
 
+class Boton extends Component {
+  handleClick = () => {
+    // Acciones a realizar cuando se hace clic en el botón
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        Texto del botón
+      </button>
+    );
+  }
+}
+
+export default Boton;
